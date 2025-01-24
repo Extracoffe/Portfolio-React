cd0d28342aef28f7191c86ba568c633332313642
André <andrerodartematos@gmail.com>
2025-01-24 19:50:16 -0300

    Comecei o portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+      <nav>
+        <div>Home</div>
+        <div>Sobre Mim</div>
+        <div>Trabalhos</div>
+        <div>Contato</div>
+      </nav>
+    </header>
+    <main>
+      <div className='App'>
+        <h1>Opa! Tudo bem?</h1>
+        <h3>Prazer! Meu nome é André Rodarte, e eu sou um desenvolvedor Front-End. Comecei a focar nesse ramo há pouco tempo, porém, em 2022 eu comecei uma faculdade de Engenharia da Computação, na Facens. Atualmente estou cursando Análise e Desenvolvimento de Sistemas na Unicesumar EAD. Estou no segundo semestre do curso, e estou totalmente empolgado para me tornar um desenvolvedor Full-Stack!</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main> 
     </>
   )
 }
